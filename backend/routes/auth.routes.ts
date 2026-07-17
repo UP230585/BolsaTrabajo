b8c83bd43febbc111ecb3220d3ddd62861db98f5
UP230585 <UP230585@alumnos.upa.edu.mx>
2026-07-17 15:56:18 -0600

    Revert "feat: registro, login, JWT y middleware de roles"

--- backend/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { authController } from "../controllers/auth.controller";
-
-export const authRouter = Router();
-
-authRouter.post("/register/estudiante", authController.registrarEstudiante);
-authRouter.post("/register/empresa", authController.registrarEmpresa);
-authRouter.post("/login", authController.login);
